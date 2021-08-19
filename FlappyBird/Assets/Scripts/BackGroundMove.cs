ea6e7926bf2475841631febb8fcc2ce3c71e2006
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2021-08-19 15:28:04 +0700

    :sparkles: create C# script

---
+++ FlappyBird/Assets/Scripts/BackGroundMove.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class BackGroundMove : MonoBehaviour
+{
+    public float moveSpeed;
+    public float moveRange;
+
+    private Vector3 oldPosition;
+    private GameObject obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = gameObject;
+        oldPosition = obj.transform.position;
+        moveSpeed = 5;
+        moveRange = 20;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        obj.transform.Translate(new Vector3(-1 * Time.deltaTime * moveSpeed, transform.position.y, transform.position.z));
+
+        if (Vector3.Distance(oldPosition, obj.transform.position) > moveRange)
+        {
+            obj.transform.position = oldPosition;
+        }
+    }
+}
