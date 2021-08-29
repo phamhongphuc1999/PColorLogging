95b65125de87961fe5ef73e98b8aa70d7bcdcf13
phamhongphuc1999 <phamhongphuc12atm1@gmail.com>
2021-08-29 14:40:53 +0700

    :pencil2: create collisition

---
+++ FlappyBird/Assets/Scripts/BirdController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class BirdController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("EndGame");
+    }
+
+    void OnCollisionStay()
+    {
+
+    }
+
+    void OnCollisionExit()
+    {
+
+    }
+
+    void OnTriggerEnter()
+    {
+
+    }
+
+    void OnTriggerStay()
+    {
+
+    }
+
+    void OnTriggerExit()
+    {
+
+    }
+}
---
+++ FlappyBird/Assets/Scripts/WallMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class WallMove : MonoBehaviour
+{
+    public float moveSpeed;
+
+    private Vector3 oldPosition;
+    private GameObject obj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj = gameObject;
+        oldPosition = obj.transform.position;
+        moveSpeed = 5;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        obj.transform.Translate(new Vector3(-1 * Time.deltaTime * moveSpeed, 0, 0));
+    }
+}
